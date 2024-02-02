8a6871a41a010d1c97632d771b1b0589d35327ef
ArturHAlves <arthenrdev@gmail.com>
2024-02-02 01:12:32 -0300

    chore: Ajuste footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -54,7 +54,7 @@ export default function Footer() {
         spacing={4}
         justify={{ base: 'center', md: 'space-between' }}
         align={{ base: 'center', md: 'center' }}>
-        <Text>© 2023 Artur Henrique</Text>
+        <Text>© 2024 Artur Henrique</Text>
         <Stack direction={'row'} spacing={6}>
           <SocialButton label={'Twitter'} href={'#'}>
             <FaTwitter />
